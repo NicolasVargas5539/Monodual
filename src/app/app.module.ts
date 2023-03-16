ad52466f320a96107a120f436dea691e53f91413
Nicolas Vargas Ch <nicolas.vargas1085@gmail.com>
2023-03-16 18:37:13 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { LoginComponent } from './monodual/pages/login/login.component';
+import { MonodualModule } from './monodual/monodual.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharedModule,
+    MonodualModule
   ],
   providers: [],
   bootstrap: [AppComponent]
